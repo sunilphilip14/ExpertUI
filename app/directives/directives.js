6a8da9abce787c8270982a3f32baa1bf1ec5ab19
Martin Vach <mv@zwaveeurope.com>
2017-08-04 16:19:43 +0200

    Queue in bottom view

--- app/directives/directives.js
+++ app/directives/directives.js
@@ -802,6 +802,63 @@ angApp.directive('bbKeyEvent', function () {
         });
     };
 });
+/**
+ * Drag horizontal and vertical div dividers
+ * https://github.com/chevalierc/angular-div-resizer
+ * https://stackoverflow.com/questions/18368485/angular-js-resizable-div-directive
+ * @class resizer
+ */
+angApp.directive('resizer', function($document) {
+
+    return function($scope, $element, $attrs) {
+
+        $element.on('mousedown', function(event) {
+            event.preventDefault();
+
+            $document.on('mousemove', mousemove);
+            $document.on('mouseup', mouseup);
+        });
+
+        function mousemove(event) {
+
+            if ($attrs.resizer == 'vertical') {
+                // Handle vertical resizer
+                var x = event.pageX;
+
+                if ($attrs.resizerMax && x > $attrs.resizerMax) {
+                    x = parseInt($attrs.resizerMax);
+                }
+
+                if ($attrs.resizerMax && x < $attrs.resizerMin) {
+                    x = parseInt($attrs.resizerMin);
+                }
+
+                $element.css({
+                    left: x + 'px'
+                });
+
+                document.getElementById($attrs.resizerLeft.substring(1)).style.width = x + 'px'
+                document.getElementById($attrs.resizerRight.substring(1)).style.left = (x + parseInt($attrs.resizerWidth)) + 'px'
+
+            } else {
+                // Handle horizontal resizer
+                var y = window.innerHeight - event.pageY;
+
+                $element.css({
+                    bottom: y + 'px'
+                });
+
+                document.getElementById($attrs.resizerTop.substring(1)).style.bottom = y + 'px'
+                document.getElementById($attrs.resizerBottom.substring(1)).style.height = (y + parseInt($attrs.resizerHeight)) + 'px'
+            }
+        }
+
+        function mouseup() {
+            $document.unbind('mousemove', mousemove);
+            $document.unbind('mouseup', mouseup);
+        }
+    };
+});
 
 /*** Fixes ***/
 // js holder fix
